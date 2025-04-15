c036093db1984aea80527146990893a32ec8e536
jibongpark <bogjibong22@naver.com>
2025-04-15 15:04:47 +0900

    ci 수정
     Default workflow만 [archive] 태그 확인 하도록 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,10 +4,11 @@ cd ..
 
 commit_message=$(git log -1 --pretty=%B)
 
-# 특정 키워드가 있는지 확인한다.
-if [[ "$commit_message" != *"[archive]"* ]]; then
-    echo "키워드가 없으므로 빌드를 중단합니다."
-    exit 0
+echo "❗️ commit : $commit_message"
+
+if [[ "$CI_WORKFLOW" = "Default" && "$commit_message" != *"[archive]"* ]]; then
+    echo "[archive] 키워드가 없으므로 빌드를 중단합니다."
+    exit 1
 fi
 
 
